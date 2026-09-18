dd97dd637b7e9c44053674815a92ee87f100e1b6
Sajeel Nadeem <sajeel.nadeem05@gmail.com>
2026-09-18 11:29:00 +0500

    Implement isSorted function

--- Task5.cpp
+++ Task5.cpp
@@ -2,9 +2,14 @@
 #include <cassert>
 using namespace std;
 
-// Stub implementation that will cause the test to fail initially
+// Implemented basic logic
 bool isSorted(const int* arr, const int size) {
-	return false;
+	for (int i = 0; i < size - 1; i++) {
+		if (arr[i] > arr[i + 1]) {
+			return false;
+		}
+	}
+	return true;
 }
 
 void testSortedArray() {
@@ -12,8 +17,20 @@ void testSortedArray() {
 	assert(isSorted(arr, 5) == true);
 }
 
+void testUnsortedArray() {
+	int arr[] = { 1, 4, 3, 5, 6 };
+	assert(isSorted(arr, 5) == false);
+}
+
+void testDuplicateValues() {
+	int arr[] = { 1, 2, 2, 3, 5 };
+	assert(isSorted(arr, 5) == true);
+}
+
 int main() {
 	testSortedArray();
-	cout << "Initial test complete." << endl;
+	testUnsortedArray();
+	testDuplicateValues();
+	cout << "Core tests passed." << endl;
 	return 0;
 }
